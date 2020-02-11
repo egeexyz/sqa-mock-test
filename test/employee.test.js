7a5451321760bb355c5e4c7c1da825ee1a5303c4
egee-irl <brian@egee.io>
2020-02-11 12:19:15 -0800

    added mocha and tests

---
+++ test/employee.test.js
@@ -0,0 +1,11 @@
+/* eslint no-undef: "off" */
+// var assert = require('assert')
+
+describe(' Add Employee', () => {
+  it('Add Employee with Discount', () => {
+    console.log('uno')
+  })
+  it('Add Employee without Discount', () => {
+    console.log('dos')
+  })
+})
