673d93dc26e4396a9cd97fce97563dce288b0f0f
egee-irl <brian@egee.io>
2020-02-11 15:27:04 -0800

    edit tests

--- test/employee.test.js
+++ test/employee.test.js
@@ -23,7 +23,6 @@ Browser.localhost('localhost', 8080)
 //     await AddEmployee(browser, employee)
 //     browser.assert.text('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)', employee.firstName)
 //     browser.assert.text('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)', employee.lastName)
-//     browser.assert.text('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)', employee.dependants)
 //     await browser.assert.elements('.glyphicon-wrench', 2)
 //   })
 //   it('Should add correctly calculate discounts employee', async () => {
@@ -89,10 +88,6 @@ Browser.localhost('localhost', 8080)
 // })
 
 describe('Edit Employee', () => {
-
-})
-
-describe('Remove Employee', () => {
   const browser = new Browser()
   beforeEach(async () => {
     employee = {
@@ -102,17 +97,73 @@ describe('Remove Employee', () => {
     }
     await Login(browser)
   })
-  it('Should remove employee', async () => {
-    employee.firstName = 'Remove'
-    employee.lastName = 'Me'
-    employee.dependants = '3'
+  it('Should edit existing employee', async () => {
+    employee.firstName = 'Zack'
+    employee.lastName = 'Siler'
+    employee.dependants = '1'
+
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(2)', employee.firstName)
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(3)', employee.lastName)
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(5)', employee.dependants)
+
+    employee.firstName = 'Zach'
+    employee.lastName = 'Sillier'
+    employee.dependants = '2'
+
+    await browser.click('#employee-table > tbody > tr td:nth-child(9) > span#btnEdit')
+    await FillEmployeeForm(browser, employee)
+    await browser.pressButton('Submit')
+
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(2)', employee.firstName)
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(3)', employee.lastName)
+    await browser.assert.text('#employee-table > tbody > tr td:nth-child(5)', employee.dependants)
+  })
+  it('Should edit new employee', async () => {
+    employee.firstName = 'Burger'
+    employee.lastName = 'Ville'
+    employee.dependants = '2'
 
     await AddEmployee(browser, employee)
-    await browser.assert.text('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)', employee.firstName)
-    // await browser.click('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(9) >span#btnDelete')
-    // browser.assert.elements('.glyphicon-wrench', 1)
+
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(2)', employee.firstName)
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(3)', employee.lastName)
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(5)', employee.dependants)
+
+    employee.firstName = 'Five'
+    employee.lastName = 'Guys'
+    employee.dependants = '5'
+
+    await browser.click('#employee-table > tbody > tr:nth-child(2) td:nth-child(9) > span#btnEdit')
+    await FillEmployeeForm(browser, employee)
+    await browser.pressButton('Submit')
+
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(2)', employee.firstName)
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(3)', employee.lastName)
+    await browser.assert.text('#employee-table > tbody > tr:nth-child(2) td:nth-child(5)', employee.dependants)
   })
 })
+
+// describe('Remove Employee', () => {
+//   const browser = new Browser()
+//   beforeEach(async () => {
+//     employee = {
+//       firstName: '',
+//       lastName: '',
+//       dependants: ''
+//     }
+//     await Login(browser)
+//   })
+//   it('Should delete employee', async () => {
+//     employee.firstName = 'Remove'
+//     employee.lastName = 'Me'
+//     employee.dependants = '3'
+
+//     await AddEmployee(browser, employee)
+//     await browser.assert.text('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(2)', employee.firstName)
+//     // await browser.click('#employee-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(9) >span#btnDelete')
+//     // browser.assert.elements('.glyphicon-wrench', 1)
+//   })
+// })
 
 const Login = async (browser) => {
   await browser.visit('/login')
@@ -123,8 +174,12 @@ const Login = async (browser) => {
 
 const AddEmployee = async (browser, employee) => {
   await browser.pressButton('#btnAddEmployee')
+  await FillEmployeeForm(browser, employee)
+  await browser.pressButton('Submit')
+}
+
+const FillEmployeeForm = async (browser, employee) => {
   await browser.fill('#employees-form .form-group:nth-child(1) .form-control', employee.firstName)
   await browser.fill('#employees-form .form-group:nth-child(2) .form-control', employee.lastName)
   await browser.fill('#employees-form .form-group:nth-child(3) .form-control', employee.dependants)
-  await browser.pressButton('Submit')
 }
